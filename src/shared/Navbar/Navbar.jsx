f2268ad1a4f0bdd41a82a61ea11bb30f8d3ac221
Darshan Hande <darshanhande11@gmail.com>
2022-08-09 19:54:41 +0530

    added navbar

---
+++ src/shared/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Navbar.css'
+import { Menu, Button } from 'antd';
+import { ClusterOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+
+const Navbar = () => {
+  return (
+    <div className='nav-div'>
+        <Menu className='navbar' mode="horizontal">
+            <div className='nav-logo-div'>
+                <ClusterOutlined className='nav-logo' />
+                <span className='logo-heading'>
+                    Cluster Protocol
+                </span>
+            </div>
+            <div className='navbar-btn-div'>
+                <Button type='primary' className='nav-btn'>
+                    Create Pool
+                </Button>
+                <Button className='nav-btn'>
+                    Connect
+                </Button>
+            </div>
+        </Menu>
+    </div>
+  )
+}
+
+export default Navbar

c948f35a3aa45836492868f678c567601b777f63
Darshan Hande <darshanhande11@gmail.com>
2022-08-13 10:35:39 +0530

    fe-fixes

--- src/components/Dashboard/Pools/Pools.jsx
+++ src/components/Dashboard/Pools/Pools.jsx
@@ -37,50 +37,53 @@ const Pools = () => {
     </Button>
   )
   return (
-    <div className='pools-list-par'>
-      <List
-        itemLayout="vertical"
-        size="large"
-        dataSource={data}
-        renderItem={(item) => (
-          <List.Item
-            key={item.title}
-            actions={[
-              <IconText icon={FaUsers} text={item.participants} key="list-vertical-users-o" />,
-              <ActionButton text={'Add Funds'} />,
-            ]}
-            extra={
-              <img
-                width={272}
-                alt="logo"
-                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+    <div className='pools-div'> 
+        <div className='pools-list-par'>
+        <h1 className='pools-heading'>Your Pools</h1>
+        <List
+          itemLayout="vertical"
+          size="large"
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item
+              key={item.title}
+              actions={[
+                <IconText icon={FaUsers} text={item.participants} key="list-vertical-users-o" />,
+                <ActionButton text={'Add Funds'} />,
+              ]}
+              extra={
+                <img
+                  width={272}
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                />
+              }
+            >
+              <List.Item.Meta
+                title={<Link to={`/pools/${item.id}`}>{item.title}</Link>}
               />
-            }
-          >
-            <List.Item.Meta
-              title={<Link to={`/pools/${item.id}`}>{item.title}</Link>}
-            />
-            {item.funds}
-          </List.Item>
-        )}
-      />
-      <Modal 
-        title="Add Funds"
-        visible={isVis}
-        onOk={addFunds}
-        onCancel={()=>{setVis(false)}}
-      >
-        <Form className='addfunds-modal-form' form={form} layout="vertical">
-          <Form.Item>
-            <Input 
-              type="number" 
-              min="0.000000000000000001"
-              placeholder="Enter amount in ETH"
-              step="0.000000000000000001" 
-            />
-          </Form.Item>
-        </Form>
-      </Modal>
+              {item.funds}
+            </List.Item>
+          )}
+        />
+        <Modal 
+          title="Add Funds"
+          visible={isVis}
+          onOk={addFunds}
+          onCancel={()=>{setVis(false)}}
+        >
+          <Form className='addfunds-modal-form' form={form} layout="vertical">
+            <Form.Item>
+              <Input 
+                type="number" 
+                min="0.000000000000000001"
+                placeholder="Enter amount in ETH"
+                step="0.000000000000000001" 
+              />
+            </Form.Item>
+          </Form>
+        </Modal>
+      </div>
     </div>
   )
 }

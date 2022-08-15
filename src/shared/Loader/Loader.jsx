eb55490048277cf595b08453356d41aa9cd856a5
Darshan Hande <darshanhande11@gmail.com>
2022-08-15 11:26:59 +0530

    center align loader

--- src/shared/Loader/Loader.jsx
+++ src/shared/Loader/Loader.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import { ThreeCircles } from 'react-loader-spinner'
+import './Loader.css'
 
 const Loader = (props) => {
   return (
-    <ThreeCircles
-        height={props.height}
-        width={props.width}
-        color={props.color ? props.color : "#00BFFF"}
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-        ariaLabel="three-circles-rotating"
-        outerCircleColor=""
-        innerCircleColor=""
-        middleCircleColor=""
-    />
+    <div className='loader-div'>
+      <ThreeCircles
+          height={props.height}
+          width={props.width}
+          color={props.color ? props.color : "#00BFFF"}
+          wrapperStyle={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="three-circles-rotating"
+          outerCircleColor=""
+          innerCircleColor=""
+          middleCircleColor=""
+      />
+    </div>
   )
 }
 
